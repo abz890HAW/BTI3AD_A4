790ca74b6c1f6369d20d0f0908fb2b0f7b14d95b
Hauke <hauke.goldhammer@haw-hamburg.de>
2017-04-13 17:41:09 +0200

    3. ans Interface angepasst

--- src/PascalschesDreieck.java
+++ src/PascalschesDreieck.java
@@ -3,29 +3,22 @@
 import java.math.BigInteger;
 import java.util.Arrays;
 
-public class PascalschesDreieck {
+public class PascalschesDreieck implements PascalGen {
 	
-	public Counter PascalschSchnellCounter;
-	
-	
-
-	public PascalschesDreieck() {
-		PascalschSchnellCounter = new Counter();
-	}
-
+	public Counter counter;
 
 	public BigInteger[] schnellPascalschesDreieckZeile(int n) {
 		BigInteger[] fakultaeten = new BigInteger[n+1];
 		fakultaeten[0] = BigInteger.ONE;
 		for(int i = 1; i < fakultaeten.length;i++ ){
 			fakultaeten[i] = fakultaeten[i-1].multiply(BigInteger.valueOf(i));
-			PascalschSchnellCounter.countUp();
+			counter.countUp();
 		}
 		
 		BigInteger[] koeffizienten = new BigInteger[n + 1];
 		for (int i = 0; i < koeffizienten.length; i++) {
 			koeffizienten[i] = fakultaeten[n].divide(fakultaeten[i].multiply(fakultaeten[n-i]));
-			PascalschSchnellCounter.countUp();
+			counter.countUp();
 		}
 		return koeffizienten;
 	}
@@ -33,11 +26,18 @@ public class PascalschesDreieck {
 
 	public static void main(String[] args) {
 		PascalschesDreieck pas = new PascalschesDreieck();
-		pas.PascalschSchnellCounter.resetCount();
+		pas.counter.resetCount();
 		for (int i = 1; i <= 10; i++) {
 			BigInteger[] koeff = pas.schnellPascalschesDreieckZeile(i);
 			System.out.println(Arrays.toString(koeff));;
 		}
-		System.out.println("Counter: " + pas.PascalschSchnellCounter.getCount());
+		System.out.println("Counter: " + pas.counter.getCount());
+	}
+
+
+	@Override
+	public BigInteger[] generatePascalLine(int n, Counter counter) {
+		this.counter = counter;
+		return schnellPascalschesDreieckZeile(n);
 	}
 }
